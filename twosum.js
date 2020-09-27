786a96a7d4c7a5e0045e75e349af1c732e669e27
vats2048 <42840800+vats2048@users.noreply.github.com>
2020-09-27 23:14:57 +0530

    two sum problem

---
+++ twosum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+   let complements = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (complements.has(complement)) {
+            return [complements.get(complement), i];
+        }
+        complements.set(nums[i], i);
+    }
+    return [];
+};
